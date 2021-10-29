7e21a4ec5852c1bb0fbeb82696888a60f7e4221e
Pedro Henrique Ramos <pedrohramos.ti@gmail.com>
2021-10-28 23:50:22 -0300

    remake of cards management

--- Memorizando/src/app/core/core.module.ts
+++ Memorizando/src/app/core/core.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ImagemComponent } from './imagem/imagem.component';
 import { SocketComponent } from './socket/socket.component';
 import { CardManagerComponent } from './card-manager/card-manager.component';
 
@@ -8,7 +7,6 @@ import { CardManagerComponent } from './card-manager/card-manager.component';
 
 @NgModule({
   declarations: [
-    ImagemComponent,
     SocketComponent,
     CardManagerComponent
   ],
